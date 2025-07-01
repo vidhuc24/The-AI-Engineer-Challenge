fe94898087f4059e1128e86ea00fbe959ce33ac3
vidhuc24 <vidhuchillara@gmail.com>
2025-06-30 20:11:55 -0500

    fix: Initial attempts to fix code highlighting and math rendering

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import 'katex/dist/katex.min.css';
 
-const geistSans = Geist({
+const geist = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -24,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geist.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
